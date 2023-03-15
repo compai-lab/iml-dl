679faed7c2d87876f62b0eb3b9d78d09b5dd4fbe
Bercea <cosmin.bercea@MacBook-Pro.fritz.box>
2023-03-15 15:36:22 +0100

    extended the read function to different formats, added more transforms

--- transforms/preprocessing.py
+++ transforms/preprocessing.py
@@ -3,7 +3,10 @@ import torch
 from monai.transforms import Transform
 from monai.utils.enums import TransformBackends
 from monai.config.type_definitions import NdarrayOrTensor
+import torchvision
 from torchvision.io.image import read_image
+import torch.nn.functional as F
+
 
 
 class ReadImage(Transform):
@@ -17,7 +20,56 @@ class ReadImage(Transform):
         """
         Apply the transform to `img`.
         """
-        return read_image(path)
+        if '.npy' in path:
+            # return torch.tensor(np.flipud((np.load(path).astype(np.float32)).T).copy())  # Mid Axial slice of MRI brains
+            img = np.load(path).astype(np.float32)
+            img = (img * 255).astype(np.uint8)
+            return torch.tensor(img)
+        elif '.jpeg' in path or '.jpg' in path or '.png' in path:
+            return read_image(path)
+        elif '.nii.gz' in path:
+            import nibabel as nip
+            from nibabel.imageglobals import LoggingOutputSuppressor
+            with LoggingOutputSuppressor():
+                img_obj = nip.load(path)
+                img_np = np.array(img_obj.get_fdata(), dtype=np.float32)
+                img_t = torch.Tensor(img_np.copy())
+                # img_t = torch.Tensor(np.flipud(img_np[:, :, 95].T).copy()) # Mid Axial slice of MRI brains
+            return img_t
+        elif '.nii' in path:
+            import nibabel as nip
+            img = nip.load(path)
+            return torch.Tensor(np.array(img.get_fdata()))
+        elif '.dcm' in path:
+            from pydicom import dcmread
+            ds = dcmread(path)
+            return torch.Tensor(ds.pixel_array)
+        elif '.h5' in path:  ## !!! SPECIFIC TO FAST MRI, CHANGE FOR OTHER DATASETS
+            import h5py
+            f = h5py.File(path, 'r')
+            img_data = f['reconstruction_rss'][:] # Fast MRI Specific
+            img_data = img_data[:, ::-1, :][0]  # flipped up down
+            return torch.tensor(img_data.copy())
+        else:
+            raise IOError
+
+
+class Norm98:
+    def __init__(self, max_val=255.0):
+        self.max_val = max_val
+        super(Norm98, self).__init__()
+
+    def __call__(self, img):
+        """
+        Apply the transform to `img`.
+        """
+        # print(torch.min(img), torch.max(img))
+        # print(img.shape)
+        q = np.percentile(img, 98)
+        img = img / q
+        img[img > 1] = 1
+        # return img/self.max_val
+        return img
 
 
 class To01:
@@ -35,6 +87,39 @@ class To01:
         """
         return img/self.max_val
 
+
+
+class AdjustIntensity:
+    def __init__(self):
+        self.values = [1, 1, 1, 0.6, 0.7, 0.8, 0.9, 1.1, 1.2, 1.3, 1.4]
+        # self.methods = [0, 1, 2]
+    def __call__(self, img):
+        value = np.random.choice(self.values)
+        # method = np.random.choice(self.methods)
+        # if method == 0:
+        return torchvision.transforms.functional.adjust_gamma(img, value)
+
+
+class Binarize:
+    def __init__(self, th = 0.5):
+        self.th = th
+        super(Binarize, self).__init__()
+
+    def __call__(self, img):
+        img[img > self.th] = 1
+        img[img < 1] = 0
+        return img
+
+
+class MinMax:
+    """
+    Min Max Norm
+    """
+    def __call__(self, img):
+        max =  torch.max(img)
+        min = torch.min(img)
+        img = (img-min) / (max - min)
+        return img
 
 class ToRGB:
     """
@@ -71,7 +156,7 @@ class AddChannelIfNeeded(Transform):
         Apply the transform to `img`.
         """
         if len(img.shape) == 2:
-            print(f'Added channel: {(img[None,...].shape)}')
+            # print(f'Added channel: {(img[None,...].shape)}')
             return img[None, ...]
         else:
             return img
@@ -92,15 +177,60 @@ class AssertChannelFirst(Transform):
         if img.shape[0] == img.shape[1] and img.shape[0] != img.shape[2]:
             print(f'Permuted channels {(img.permute(2,0,1)).shape}')
             return img.permute(2, 0, 1)
+        elif img.shape[0] > 1:
+            return img[0: 1, :, :]
         else:
             return img
 
 
-def visualize_slice(img):
+class Slice(Transform):
     """
-    Normalize for visualization
+    Pad with zeros
     """
-    # img = img[0][0]
-    img[0][0] = 0
-    img[0][1] = 1
-    return img
+    backend = [TransformBackends.TORCH, TransformBackends.NUMPY]
+    def __init__(self, axis=0):
+        self.axis = axis
+
+    def __call__(self, img: NdarrayOrTensor) -> NdarrayOrTensor:
+        # x = int(320 - img.shape[0] / 2)
+        # y = int(320 - img.shape[1] / 2)
+        # self.pid = (x, y)
+        mid_slice = int(img.shape[0]/2)
+        img_slice = img[mid_slice, :, :]
+        return img_slice
+
+
+class Pad(Transform):
+    """
+    Pad with zeros
+    """
+    backend = [TransformBackends.TORCH, TransformBackends.NUMPY]
+    def __init__(self, pid= (1,1)):
+        self.pid = pid
+
+    def __call__(self, img: NdarrayOrTensor) -> NdarrayOrTensor:
+        img = torch.squeeze(img)
+        max_dim = max(img.shape[0], img.shape[1])
+        x = max_dim - img.shape[0]
+        y = max_dim - img.shape[1]
+
+        self.pid = (int(y/2), y-int(y/2), int(x/2), x-int(x/2))
+        pad_val = torch.min(img)
+        img_pad = F.pad(img, self.pid, 'constant', pad_val)
+
+        return img_pad
+
+
+class Zoom(Transform):
+    """
+    Resize 3d volumes
+    """
+    def __init__(self, input_size):
+        self.input_size = input_size
+        self.mode = 'trilinear' if len(input_size) > 2 else 'bilinear'
+    backend = [TransformBackends.TORCH, TransformBackends.NUMPY]
+
+    def __call__(self, img: NdarrayOrTensor) -> NdarrayOrTensor:
+        if len(img.shape) == 3:
+            img = img[None, ...]
+        return F.interpolate(img,  size=self.input_size, mode=self.mode)[0]
